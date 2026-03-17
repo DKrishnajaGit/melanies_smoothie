4ac537dc47a072dc3f3ee0c0c1c3316e9f5d2ea5
DKrishnajaGit <krishnaja.d@sonata-software.com>
2026-03-17 20:50:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,9 @@ st.write(f"Choose the fruits :banana: :strawberry: :pineapple: :apple: :mango: :
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on the smoothie will be: '+name_on_order)
 
-session = get_active_session() 
+# session = get_active_session() 
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'))
